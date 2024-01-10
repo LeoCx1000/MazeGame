980bdd2d2b73df20226876e2cafd3a217e2181ba
Leo <leonardocavenago05@gmail.com>
2024-01-10 15:19:21 +0100

    add more functionality to the window

--- __main__.py
+++ __main__.py
@@ -1,11 +1,30 @@
 from blessed import Terminal
 import argparse
+import unicodedata
 from maze import Maze, Player, Size, Theme, DEFAULT_THEME
 
 terminal = Terminal()
 
 maze = Maze(Size((terminal.width // 4) - terminal.width % 2, (terminal.height // 2) - terminal.height % 2 + 2))
-key_to_cardinal = {"W": "N", "A": "W", "S": "S", "D": "E"}
+key_to_cardinal = {
+    "W": "N",
+    "A": "W",
+    "S": "S",
+    "D": "E",
+    "\U0000001b\N{LEFT SQUARE BRACKET}\N{LATIN CAPITAL LETTER A}": "N",  # arrow UP
+    "\U0000001b\N{LEFT SQUARE BRACKET}\N{LATIN CAPITAL LETTER D}": "W",  # arrow RIGHT
+    "\U0000001b\N{LEFT SQUARE BRACKET}\N{LATIN CAPITAL LETTER B}": "S",  # arrow DOWN
+    "\U0000001b\N{LEFT SQUARE BRACKET}\N{LATIN CAPITAL LETTER C}": "E",  # arrow LEFT
+}
+
+
+def to_string(chars):
+    ret = ""
+    for c in chars:
+        digit = f"{ord(c):x}"
+        name = unicodedata.name(c, None)
+        ret += ("\\N{" + name + "}") if name else f"\\U{digit:>08}"
+    return ret
 
 
 def main(full_render: bool, snap: bool):
@@ -14,31 +33,55 @@ def main(full_render: bool, snap: bool):
     print(terminal.home + terminal.clear + terminal.move_y(terminal.height // 2))
     print(terminal.black_on_darkkhaki(terminal.center("Generating maze...")))
 
-    theme = Theme(
+    dark_theme = Theme(
         wall=terminal.on_black("  "),
-        tile=terminal.on_green("  "),
-        player=terminal.on_green(DEFAULT_THEME.player),
+        tile=terminal.on_gray23("  "),
+        player=terminal.on_gray23(DEFAULT_THEME.player),
         start=terminal.on_red("  "),
-        end=terminal.red_on_green("❌"),
-        up=terminal.black_on_green(DEFAULT_THEME.up),
-        down=terminal.black_on_green(DEFAULT_THEME.down),
-        left=terminal.black_on_green(DEFAULT_THEME.left),
-        right=terminal.black_on_green(DEFAULT_THEME.right),
-        trail=terminal.on_green(DEFAULT_THEME.trail),
+        end=terminal.red_on_gray23("❌"),
+        up=terminal.black_on_gray23(DEFAULT_THEME.up),
+        down=terminal.black_on_gray23(DEFAULT_THEME.down),
+        left=terminal.black_on_gray23(DEFAULT_THEME.left),
+        right=terminal.black_on_gray23(DEFAULT_THEME.right),
+        trail=terminal.on_gray23(DEFAULT_THEME.trail),
+    )
+
+    light_theme = Theme(
+        wall=terminal.on_white("  "),
+        tile=terminal.on_lightskyblue4("  "),
+        player=terminal.on_lightskyblue4(DEFAULT_THEME.player),
+        start=terminal.on_red("  "),
+        end=terminal.red_on_lightskyblue4("❌"),
+        up=terminal.black_on_lightskyblue4(DEFAULT_THEME.up),
+        down=terminal.black_on_lightskyblue4(DEFAULT_THEME.down),
+        left=terminal.black_on_lightskyblue4(DEFAULT_THEME.left),
+        right=terminal.black_on_lightskyblue4(DEFAULT_THEME.right),
+        trail=terminal.on_lightskyblue4(DEFAULT_THEME.trail),
     )
 
     maze = Maze(Size((terminal.width // 4) - 1, (terminal.height // 2) - 2))
     maze.generate()
 
+    db = terminal.white_on_dodgerblue4
+    lb = terminal.black_on_steelblue1
+
+    original_size = (terminal.width, terminal.height)
+
     header = terminal.ljust(
         terminal.white_on_blue(terminal.center(f"Maze Game (Size {maze.size.width}x{maze.size.height})"))
         + "\n"
-        + terminal.white_on_dodgerblue4(terminal.ljust(f"Use WASD to move. X to exit. This is you: {theme.player}"))
+        + "\n".join(
+            db(terminal.ljust(line))
+            for line in terminal.wrap(
+                f"{lb('WASD')} or arrows to move. {lb('Q')} to quit. This is you: {dark_theme.player}. "
+                f"{lb('T')} to toggle theme. {lb('R')} to tgl render mode. {lb('E')} to tgl snap mode."
+            )
+        )
         + "\n"
     )
 
     with terminal.cbreak(), terminal.hidden_cursor():
-        player = Player(maze=maze, theme=theme)
+        player = Player(maze=maze, theme=dark_theme)
         player.snap = snap
 
         render_func = player.full_render if full_render else player.render
@@ -46,14 +89,42 @@ def main(full_render: bool, snap: bool):
         print(terminal.home + terminal.clear + header)
         render_func(lpadding="  ")
 
-        user_input = None
-        while (not user_input or user_input.lower() != "x") and not player.won:
+        while not player.won:
             user_input = terminal.inkey().upper()
-            if user_input in ["W", "A", "S", "D"]:
-                player.move(key_to_cardinal[user_input])
-                render_func(lpadding="  ", console_clear_sequence=terminal.home + terminal.clear + header)
 
-        if player.won:
+            if original_size != (terminal.width, terminal.height):
+                print(terminal.home + terminal.clear)
+                print(terminal.white_on_red(terminal.center(" ")))
+                print(terminal.white_on_red(terminal.center("Terminal window was resized. Try again.")))
+                print(terminal.white_on_red(terminal.center(" ")))
+                break
+
+            if user_input == "Q":
+                print(terminal.home + terminal.clear)
+                print(terminal.white_on_red(terminal.center(" ")))
+                print(terminal.white_on_red(terminal.center("You quit.")))
+                print(terminal.white_on_red(terminal.center(" ")))
+                break
+
+            footer = None
+            try:
+                player.move(key_to_cardinal[user_input])
+            except KeyError:
+                if user_input == "T":
+                    player.theme = light_theme if player.theme == dark_theme else dark_theme
+                elif user_input == "R":
+                    render_func = player.render if render_func == player.full_render else player.full_render
+                elif user_input == "E":
+                    player.snap = not player.snap
+                else:
+                    footer = terminal.black_on_red(terminal.center(f"Unknown key: {to_string(user_input)}"))
+
+            render_func(lpadding="  ", console_clear_sequence=terminal.home + terminal.clear + header)
+
+            if footer:
+                print(footer)
+
+        else:
             print(terminal.home + terminal.clear)
             print(terminal.black_on_lime(terminal.center(" ")))
             print(terminal.black_on_lime(terminal.center("Congratulations. You won!")))
@@ -62,7 +133,7 @@ def main(full_render: bool, snap: bool):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="MazeGame", description="A fun little maze game")
-    parser.add_argument("-f", "--full", action="store_true")
-    parser.add_argument("-s", "--snap", action="store_true")
+    parser.add_argument("-pv", "--partial", action="store_false", help="Pass to start in partial visibility mode.")
+    parser.add_argument("-s", "--snap", action="store_true", help="Pass to start with snap movement mode.")
     flags = parser.parse_args()
-    main(full_render=flags.full, snap=flags.snap)
+    main(full_render=flags.partial, snap=flags.snap)
